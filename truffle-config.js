323ed4b323260a827f768205a87b590c064df00d
“ariel.duarte” <arielpy@gmail.com>
2025-10-27 09:10:49 -0300

    Add CustomERC20 contract with minting and burning functions, initial migration, and tests

--- truffle-config.js
+++ truffle-config.js
@@ -69,6 +69,7 @@ module.exports = {
     //  port: 8545,            // Standard Ethereum port (default: none)
     //  network_id: "*",       // Any network (default: none)
     // },
+    // $ truffle test --network ganache
     ganache: {
       host: "127.0.0.1", // Localhost (default: none)
       port: 7545, // Standard Ethereum port (default: none)
